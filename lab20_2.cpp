b6054bc66bade134b229b00ff8fdfa29f2f6d1f0
tensept <110962972+tensept@users.noreply.github.com>
2023-02-21 16:37:16 +0700

    lab 20_2 completed

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
+double overlap(Rect *R1, Rect *R2){
+    double Area1,Area2,total;
+    Area1 = min((*R1).x+(*R1).w,(*R2).x+(*R2).w)-max((*R1).x,(*R2).x);
+    Area2 = min((*R1).y,(*R2).y)- max((*R1).y-(*R1).h,(*R2).y-(*R2).h);
+    total = Area1*Area2 ;
+    if(total < 0) total = 0;
+    return total;
 
 }
